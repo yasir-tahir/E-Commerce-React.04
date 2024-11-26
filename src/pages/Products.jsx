e603afaa0962dfd9a5ac6665eadb95bb43106464
Muhammad Yasir <yasirtahir009@gmail.com>
2024-11-27 01:55:38 +0500

    commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -27,7 +27,37 @@ useEffect(()=> {
 },[]);
 
 
-console.log("products", products);
+// console.log("products", products);
+
+const [searchTerm, setSearchTerm] = useState("");
+
+console.log(searchTerm);
+
+const searchProducts  = () => {
+  const result = products?.filter((item) => {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+  console.log('result', result);
+  
+  return result || []
+};
+
+const searchResult = searchProducts()
+
+
+// is code mai ap price or discount ek hi tarike ke match karwaskte hai 
+// const searchProducts  = () => {
+//   const result = products?.filter((item) => {
+//     return ;
+//   });
+//   console.log('result', result);
+  
+//   return result || []
+// };
+
+// const searchResult = searchProducts()
+
+
   return (
     <div className="container">
       
@@ -35,14 +65,14 @@ console.log("products", products);
        name="price" 
        id="price"
        onChange={(event)=>{
-        console.log("value", event.target.value);
+        setSearchTerm(event.target.value);
         
        }}
         class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 outline-none text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary my-8 sm:text-sm/6" placeholder="0.00" />
   
       
       <div className=" flex flex-wrap gap-4 center"> 
-        {products?.map((item) => (
+        {searchResult?.map((item) => (
           <ProductCard  key={item.id} image={item.thumbnail} percent={item.discountPercentage} text={item.
              title}  price={item.price} discountPercentage={item.discountPercentage} star={stars}
              />  
